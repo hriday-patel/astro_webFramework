437534e76fa1e1856f52e5f697322860ca5c0e36
hriday-patel <patelhriday23@gmail.com>
2025-10-27 18:51:52 +0530

    published

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import netlify from '@astrojs/netlify';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -12,5 +14,5 @@ export default defineConfig({
   },
 
   output: 'server',
-  adapter: netlify()
+  adapter: vercel()
 });
