db3035fa16ddfa574e57d3c4e745ade2eda6df9b
hriday-patel <patelhriday23@gmail.com>
2025-10-27 18:35:32 +0530

    ssr and ssg enabled and ready to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  output: 'server'
+
+  output: 'server',
+  adapter: netlify()
 });
